822fead0f4fcafe7e4a80e0b1a99b941f43a1538
Roshanak Agahi <rosh.agahi@gmail.com>
2019-12-19 22:43:35 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -6,7 +6,13 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   def down 
   end 
   
-  def change 
+  def change
+    create_table :artists do |t|
+      t.string :name 
+      t.string :genre 
+      t.integer :age 
+      t.string :hometown
+    end
   end 
   
 end
